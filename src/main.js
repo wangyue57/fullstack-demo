0b686fd434ed6758d377ebaaa5942c1c85c8600d
王跃 <hust_wangyue@foxmail.com>
2017-11-28 11:14:04 +0800

    url

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import store from './store'
-import '@/lib'
+import router from './router'
+import './lib'
 
 Vue.config.productionTip = false
 
